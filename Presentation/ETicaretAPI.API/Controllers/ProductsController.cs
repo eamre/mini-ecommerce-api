ed430f78541f82c25230842e8c399191cabe06ea
eamre <emrealkan.1@outlook.com>
2023-10-06 21:59:42 +0300

    ders25

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -15,18 +15,20 @@ namespace ETicaretAPI.API.Controllers
     {
         private readonly IProductWriteRepository _productWriteRepository;
         private readonly IProductReadRepository _productReadRepository;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IWebHostEnvironment hostingEnvironment)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery]Pagination pagination)
+        public IActionResult GetAll([FromQuery] Pagination pagination)
         {
             var totalProducts = _productReadRepository.GetAll(false).Count();
-            var products = _productReadRepository.GetAll(false).Skip(pagination.Page*pagination.Size)
+            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size)
                 .Take(pagination.Size)
                 .Select(p => new
                 {
@@ -38,7 +40,7 @@ namespace ETicaretAPI.API.Controllers
                     p.UpdatedDate
                 });
 
-            return Ok(new {totalProducts,products});
+            return Ok(new { totalProducts, products });
         }
 
         [HttpGet("{id}")]
@@ -83,5 +85,28 @@ namespace ETicaretAPI.API.Controllers
             return Ok();
         }
 
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            string uploadPath = Path.Combine(_hostingEnvironment.WebRootPath, "resource/product-images");
+            
+            if(!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            foreach(IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, 
+                    $"{Guid.NewGuid().ToString()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = 
+                    new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024* 1024);
+
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
+        }
     }
 }
